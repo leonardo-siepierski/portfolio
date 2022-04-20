e3b3734de179eb90eaa58a64e115f8ad9c224d91
leonardo <leonardo.siepierski@outlook.com>
2022-04-20 16:37:00 -0300

    Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,31 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        {/* <LinkColumn>
+          <LinkTitle>WhatsApp</LinkTitle>
+          <LinkItem href="https://wa.me/5511993648141">(11) 99364-8141</LinkItem>
+        </LinkColumn> */}
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:leonardo.siepierski@outlook.com">leonardo.siepierski@outlook.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Leonardo Siepierski &copy; 2022</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href="https://github.com/leonardo-siepierski/" target="_blank">
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://www.linkedin.com/in/leonardo-siepierski/" target="_blank">
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
